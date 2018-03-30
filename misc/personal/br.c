de088142eac61410efc3d22d431202dbca711504
ilyas <lenovo@g510>
2018-03-31 00:04:54 +0600

    small changes in br.c

--- misc/personal/br.c
+++ misc/personal/br.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_FNAME "/sys/class/backlight/acpi_video0/max_brightness"
 #define CUR_FNAME "/sys/class/backlight/acpi_video0/brightness"
 
-void usage();
-unsigned int get_value(const char*);
-void change_value(const char*, unsigned int);
+unsigned int get_value(char*);
+void change_value(char*, unsigned int);
+
+static const char *usage = "Single argument needed:\n\
+info - show current; up - increase; down - decrease brightness.\n";
 
 int
 main (int argc, char *argv[])
 {
     if (argc != 2) {
-        usage();
-        return 1;
+        fprintf(stderr, usage);
+        exit(1);
     } else {
         if (strcmp(argv[1], "info") != 0 &&
                 strcmp(argv[1], "up") != 0 &&
                 strcmp(argv[1], "down") != 0) {
-            usage();
-            return 1;
+            fprintf(stderr, usage);
+            exit(1);
         }
     }
     unsigned int max, cur;
@@ -28,36 +31,40 @@ main (int argc, char *argv[])
     if (strcmp(argv[1], "up") == 0) {
         if (cur != max)
             change_value(CUR_FNAME, ++cur);
+        else
+            fprintf(stderr, "Already at maximum brightness.\n");
     } else if (strcmp(argv[1], "down") == 0) {
         if (cur != 1)
             change_value(CUR_FNAME, --cur);
+        else
+            fprintf(stderr, "Already at minimum brightness.\n");
     } else
-        fprintf(stdout, "%u/%u\n", cur, max);
+        fprintf(stdout, "Brightness: %u/%u.\n", cur, max);
     return 0;
 }
 
-void
-usage ()
-{
-    printf("This tool takes exactly one argument:\n\
-            info - show current brightness;\n\
-            up   - increase brightness;\n\
-            down - decrease brightness.\n\
-            Example: brcontrol up.\n");
-}
-
 unsigned int
-get_value (const char *fname)
+get_value (char *fname)
 {
-    FILE* fp = fopen(fname, "r");
+    FILE *fp = fopen(fname, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open file: %s\n", fname);
+        exit(1);
+    }
     unsigned int val;
     fscanf(fp, "%u", &val);
+    fclose(fp);
     return val;
 }
 
 void
-change_value (const char *fname, unsigned int to)
+change_value (char *fname, unsigned int val)
 {
     FILE *fp = fopen(fname, "w");
-    fprintf(fp, "%u", to);
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open file: %s\n", fname);
+        exit(1);
+    }
+    fprintf(fp, "%u", val);
+    fclose(fp);
 }
