d7bf0eab733b774d69cd6da37bdd8990b489f384
Nox <fp9821@gmail.com>
2018-11-15 15:52:21 +0900

    modify install script

--- install.sh
+++ install.sh
@@ -2,18 +2,60 @@
 
 cdir=$(pwd)
 hdir=/home/$1
+bindir=$hdir/dotfiles-2/bin
+fontdir=$hdir/dotfiles-2/misc/fonts
+
+pkg_system="xorg-server xorg-xinit xorg-xsetroot bash-completion gcc make pkg-config libx11 libxft libxinerama patch python-pip python-pywal fakeroot"
+pkg_essentials="openssh vsftpd wget git ps_mem ranger htop scrot w3m atool highlight zip unzip neovim xclip mupdf mpv surf rxvt-unicode unrar"
+pkg_gui_apps="filezilla gimp inkscape chromium firefox libreoffice-fresh"
+pkg_fonts="noto-fonts-cjk adobe-source-han-serif-otc-fonts"
+list_pkgs="$pkg_system $pkg_essentials"
+# TODO: add pkg_fonts and pkg_gui_apps when testing is done
+
+# https://wiki.archlinux.org/index.php/Fonts
+
+echo "installing Iosevka fonts"
+cd $fontdir # && wget https://aur.archlinux.org/cgit/aur.git/snapshot/ttf-iosevka.tar.gz
+#tar xzvf ttf-iosevka.tar.gz
+#cd $fontdir/ttf-iosevka && makepkg -s
+sudo pacman -U --noconfirm ttf-iosevka-2.0.1-1-any.pkg.tar.xz
 
 echo "Updating packages"
 pacman -Syu --noconfirm
 echo "Installing packages (this may take a while)"
-echo "System stuff: xorg-server xorg-xinit xorg-xsetroot bash-completion gcc make pkg-config libx11 libxft libxinerama ttf-ubuntu-font-family python-pip"
-echo "Apps: ranger htop scrot w3m atool highlight unzip neovim xclip mupdf mplayer qutebrowser transmission-qt nodejs npm urxvt nodejs npm yarn zip unrar openssh filezilla gimp inkscape chromium firefox libreoffice-fresh"
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xsetroot bash-completion gcc make pkg-config libx11 libxft libxinerama ttf-ubuntu-font-family ranger htop scrot w3m atool highlight unzip python-pip neovim xclip mupdf mplayer qutebrowser transmission-qt rxvt-unicode nodejs npm yarn zip unrar openssh filezilla gimp inkscape chromium firefox libreoffice-fresh
+echo "System stuff: $pkg_system"
+echo "Apps: $pkg_essentials $pkg_gui_apps"
+pacman -S --noconfirm $list_pkgs
+
+#echo "Installing st"
+#git clone git://git.suckless.org/st $hdir/apps
 
 echo "Moving and installing suckless apps"
 cp -Rf $cdir/apps/       $hdir/apps
 cd $hdir/apps/dwm-6.1/   && make clean install
 cd $hdir/apps/dmenu-4.8/ && make clean install
+cd $hdir/apps/st-0.8.1/  && make clean install
+
+
+echo "Installing bin files"
+#wget https://raw.githubusercontent.com/jschx/ufetch/master/ufetch-arch $hdir/uf
+# chmod +x $bindir/uf
+find $bindir/ -type f -exec chmod 755 {} \;
+cp -rf $bindir/* /usr/local/bin/
+# TODO: manage symlink with gnu stow
+
+
+echo "installing xwinwrap"
+cd $hdir && git clone https://github.com/ujjwal96/xwinwrap
+cd xwinwrap
+make && sudo make install
+make clean
+
+echo "copy dynamic wallpaper"
+#cp $hdir/dotfiles-2/misc/pixels10.gif $hdir/.cache/wallpaper.gif
+cp -rf $hdir/dotfiles-2/misc/gifs $hdir/.cache/gifs/
+# put this in xinitrc (DONE!)
+# xwinwrap -ov -ni -fs -- mpv -wid WID --keepaspect=no --loop ~/.cache/gifs/pixels10.gif
 
 echo "Placing .bashrc and .xinitrc in ~/"
 cd $cdir
@@ -23,8 +65,8 @@ cp -f misc/Xresources $hdir/.Xresources
 
 echo "Placing configs for user in a proper location"
 curl -fLo $hdir/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p $hdir/.config/{qutebrowser,ranger,nvim}
-cp -Rf config/qutebrowser/* $hdir/.config/qutebrowser/
+mkdir -p $hdir/.config/{ranger,nvim} # removed qutebrowser
+#cp -Rf config/qutebrowser/* $hdir/.config/qutebrowser/
 cp -Rf config/ranger/*      $hdir/.config/ranger/
 cp -Rf config/nvim/*        $hdir/.config/nvim/
 mkdir $hdir/{downloads,screens}
@@ -45,12 +87,17 @@ cp -f misc/bashprofile $hdir/.bash_profile
 echo 'Creating symlink for nvim called vim'
 ln -sf /bin/nvim /usr/bin/vim
 
-echo 'Installing eslint'
-npm install -g eslint
-cp -Rf misc/eslintrc.json $hdir/.eslintrc.json
+#echo 'Installing eslint'
+#npm install -g eslint
+#cp -Rf misc/eslintrc.json $hdir/.eslintrc.json
 
 echo "Changing the owner (recursively) of the home directory"
 chown -R $1 $hdir
+
+
+echo "cleaning up installation files"
+rm -rf $hdir/apps
+rm -rf $hdir/xwinwrap
 
 echo "Everything is done"
 echo "Now run in nvim :PlugInstall"
